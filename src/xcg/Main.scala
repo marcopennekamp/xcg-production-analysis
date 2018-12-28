1ee0b82accfc4ddff6696b882203218aa712d632
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-12-28 01:40:59 +0100

    Display costPerWare and costPerHour for smartchips.

--- src/xcg/Main.scala
+++ src/xcg/Main.scala
@@ -4,6 +4,9 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     Wares.load()
+    val production = Wares.get(Id("smartchips")).get.production.get
+    println(production.costPerWare)
+    println(production.costPerHour)
   }
 
 }
