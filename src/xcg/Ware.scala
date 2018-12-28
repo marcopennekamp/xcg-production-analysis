df99cbd0a0277166dfc897ebf71ea19dff6d89ae
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-12-28 15:05:15 +0100

    Add more metrics.

--- src/xcg/Ware.scala
+++ src/xcg/Ware.scala
@@ -13,18 +13,28 @@ object Price {
   val zero: Price = Price(0, 0, 0)
 }
 
-case class Production(ware: Ware, timeSeconds: Int, amount: Int, resources: Seq[Stack]) {
+case class Production(product: Ware, timeSeconds: Int, amount: Int, resources: Seq[Stack]) {
   lazy val cyclesPerHour: Double = 3600.0 / timeSeconds
   lazy val amountPerHour: Double = amount * cyclesPerHour
-  lazy val volumePerHour: Double = ware.volume * amountPerHour
+  lazy val volumePerHour: Double = product.volume * amountPerHour
 
   lazy val costPerWare: Price = resources.map(_.value).fold(Price.zero)(_ + _) / amount
   lazy val costPerHour: Price = costPerWare * amountPerHour
+
+  lazy val resourceVolumePerWare: Double = resources.map(_.volume).sum / amount
+  lazy val resourceVolumePerHour: Double = resourceVolumePerWare * amountPerHour
+
+  /**
+    * The ratio of resource volume compared to product volume. For example, a ratio of 2.0 would mean that
+    * the resources have twice the volume of the product.
+    */
+  lazy val volumeRatio: Double = resourceVolumePerWare / product.volume
 }
 
 case class Stack(wareId: Id[Ware], amount: Int) {
   lazy val ware: Ware = Wares.get(wareId).get
   lazy val value: Price = ware.price * amount
+  lazy val volume: Double = ware.volume * amount
 }
 
 case class Ware(id: Id[Ware], volume: Int, price: Price) {
