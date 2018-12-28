5d5651425a4e6682788b90c33a5368ff0cde57c3
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-12-28 01:26:10 +0100

    Add ware loading from wares.xml.

--- src/xcg/Ware.scala
+++ src/xcg/Ware.scala
@@ -1,6 +1,6 @@
 package xcg
 
-case class Price(min: Int, avg: Int, max: Int)
+case class Price(min: Int, average: Int, max: Int)
 
 case class Production(ware: Ware, timeSeconds: Int, amount: Int, resources: Seq[Stack]) {
   lazy val cyclesPerHour: Double = 3600.0 / timeSeconds
@@ -8,8 +8,10 @@ case class Production(ware: Ware, timeSeconds: Int, amount: Int, resources: Seq[
   lazy val volumePerHour: Double = ware.volume * amountPerHour
 }
 
-case class Stack(ware: Ware, amount: Int)
+case class Stack(wareId: Id[Ware], amount: Int) {
+  lazy val ware: Ware = Wares.get(wareId).get
+}
 
 case class Ware(id: Id[Ware], volume: Int, price: Price) {
-
+  var production: Option[Production] = None
 }
--- src/xcg/Wares.scala
+++ src/xcg/Wares.scala
@@ -1,12 +1,34 @@
 package xcg
 
 import scala.collection.mutable
+import scala.xml.{Node, XML}
 
 object Wares {
   private val wares: mutable.HashMap[Id[Ware], Ware] = mutable.HashMap.empty
 
   def load(): Unit = {
+    val element = XML.loadFile("wares.xml")
 
+    // Filter and only include ware nodes which have "economy" as one of their tags.
+    val wareNodes = (element \ "ware").filter { node =>
+      node.attribute("tags").fold(false)(tags => tags.head.text.contains("economy"))
+    }
+
+    def parseWare(node: Node): Ware = {
+      val priceNode = node \ "price"
+      val price = Price((priceNode \@ "min").toInt, (priceNode \@ "average").toInt, (priceNode \@ "max").toInt)
+      val ware = Ware(Id(node \@ "id"), (node \@ "volume").toInt, price)
+      val productionNode = (node \ "production").find(_ \@ "method" == "default")
+      ware.production = productionNode.map { node =>
+        val resources = ((node \ "primary") \ "ware").map { resourceNode =>
+          Stack(Id(resourceNode \@ "ware"), (resourceNode \@ "amount").toInt)
+        }
+        Production(ware, (node \@ "time").toInt, (node \@ "amount").toInt, resources)
+      }
+      ware
+    }
+
+    wareNodes.map(parseWare).foreach(ware => wares.put(ware.id, ware))
   }
 
   def get(id: Id[Ware]): Option[Ware] = wares.get(id)
