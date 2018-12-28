c679ffd90d03a6aa3f5575f7d2b2f7bc2565b378
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-12-28 01:25:58 +0100

    Add Main class.

---
+++ src/xcg/Main.scala
@@ -0,0 +1,9 @@
+package xcg
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    Wares.load()
+  }
+
+}
--- src/xcg/Wares.scala
+++ src/xcg/Wares.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable
 object Wares {
   private val wares: mutable.HashMap[Id[Ware], Ware] = mutable.HashMap.empty
 
-  def setup(): Unit = {
+  def load(): Unit = {
 
   }
 
