dea0db15f2ebcc32705a958b6e4f8b368f28422d
Marco Pennekamp <marco.pennekamp96@gmail.com>
2018-12-30 01:47:41 +0100

    Display "+percentage" for positive relative percentage values.

--- src/xcg/Formatting.scala
+++ src/xcg/Formatting.scala
@@ -12,6 +12,9 @@ object Formatting {
 
   implicit class DoubleAsPercentage(d: Double) {
     def asPercentage: String = percentInstance.format(d)
-    def asRelativePercentage: String = (d - 1.0).asPercentage
+    def asRelativePercentage: String = {
+      val change = (d - 1.0).asPercentage
+      if (d > 1.0) s"+$change" else change
+    }
   }
 }
