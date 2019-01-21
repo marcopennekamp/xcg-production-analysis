e19800a5ed635d062eb8936f25a15df8eb967b5a
Marco Pennekamp <marco.pennekamp96@gmail.com>
2019-01-21 17:30:00 +0100

    Make tdNumber accessible outside the ComparisonTable layout.

--- src/xcg/Layouts.scala
+++ src/xcg/Layouts.scala
@@ -6,6 +6,15 @@ import scalatags.Text.all._
 import Utils._
 
 object Layouts {
+  /**
+    * Formats the value in a human-readable fashion.
+    */
+  def tdNumber(value: Double, unit: Option[X4Unit] = None): Modifier = {
+    val rounded = value.formatRound
+    val valueString = if (rounded.isWhole()) rounded.toInt.toString else rounded.toString
+    unit.map(unit => td(valueString, " ", unit.toHtml)).getOrElse(td(valueString))
+  }
+
   object ComparisonTable {
     case class Column(
       header: String,
@@ -40,20 +49,11 @@ object Layouts {
           }
         }
 
-        /**
-          * Formats the value in a human-readable fashion.
-          */
-        def tdValue(value: Double, unit: Option[X4Unit]): Modifier = {
-          val rounded = value.formatRound
-          val valueString = if (rounded.isWhole()) rounded.toInt.toString else rounded.toString
-          unit.map(unit => td(valueString, " ", unit.toHtml)).getOrElse(td(valueString))
-        }
-
         table(
           tr(Seq(th(cls := "column-divider")(b(tableName))) ++ columns.map(_.header).map(th(_))),
-          tr(Seq(td(cls := "column-divider")(comparison.top.name)) ++ columns.map(c => tdValue(c.top, c.unit))),
+          tr(Seq(td(cls := "column-divider")(comparison.top.name)) ++ columns.map(c => tdNumber(c.top, c.unit))),
           tr(cls := "row-divider")(Seq(td(cls := "column-divider")(comparison.bottom.name)) ++
-            columns.map(c => tdValue(c.bottom, c.unit))),
+            columns.map(c => tdNumber(c.bottom, c.unit))),
           tr(Seq(td(cls := "column-divider")(ratioName)) ++ ratios)
         )
       }
